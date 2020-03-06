f9dd1c9ba85445508eac623e3629107d7cd81e77
wangruiCoder <893652901@qq.com>
2020-03-06 18:20:07 +0800

    增加netty

---
+++ uims-datamaintain/src/main/java/org/uims/datamaintain/netty/client/EchoClientHandler.java
@@ -0,0 +1,30 @@
+package org.uims.datamaintain.netty.client;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.util.CharsetUtil;
+
+@ChannelHandler.Sharable
+public class EchoClientHandler extends SimpleChannelInboundHandler<ByteBuf> {
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) {
+        ctx.writeAndFlush(Unpooled.copiedBuffer("Netty rocks!",
+                CharsetUtil.UTF_8) ) ;
+    }
+
+    @Override
+    public void channelRead0(ChannelHandlerContext ctx, ByteBuf in) {
+        System.out.println(
+                "Client received: " + in.toString(CharsetUtil.UTF_8));
+    }
+
+    public void exceptionCaught(ChannelHandlerContext ctx,
+                                Throwable cause) {
+        System.out.println("netty client connet server error");
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
---
+++ uims-datamaintain/src/main/java/org/uims/datamaintain/netty/server/EchoServerHandler.java
@@ -0,0 +1,34 @@
+package org.uims.datamaintain.netty.server;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.CharsetUtil;
+
+@ChannelHandler.Sharable
+public class EchoServerHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        super.channelRead(ctx, msg);
+
+        ByteBuf byteBuf = (ByteBuf) msg;
+        System.out.println(
+                "Server received: " + byteBuf.toString(CharsetUtil.UTF_8));
+        ctx.write(ctx);
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) {
+        ctx.writeAndFlush(Unpooled.EMPTY_BUFFER)
+                .addListener(ChannelFutureListener.CLOSE);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx,Throwable cause) {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
